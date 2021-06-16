c8bed59f2bb689f675f76184e35beac4f3fa8de2
Henry001-prog <alissonhenrique001@hotmail.com.br>
2021-06-16 18:44:22 -0300

    settings

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -6,6 +6,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
